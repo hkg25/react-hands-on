ee0931602c2bbb9fae0494dc023eae818a10e3bd
Harish Garg <hgarg@sapient.com>
2017-11-11 17:36:43 +0530

    redux concepts examples

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
-import {createStore} from "redux";
+import {createStore,combineReducers} from "redux";
 
 const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
 
 
-//Reducer functionm
+// Reducer functionm
 // state is passed as undefined while called by store like counterReducer(undefinded,action)
 function counterReducer(state=100,action){
     console.log("counter reducer ", state, action);
@@ -11,6 +13,10 @@ function counterReducer(state=100,action){
     switch (action.type) {
         case INCREMENT : 
             return state + action.payload.value;
+        case DECREMENT : 
+            return state + action.payload.value;
+        case RESET :
+            return 0;
         // it will return undefined if we do not put default statement 
         // means state will be lost if we have declared it.
         default:
@@ -18,16 +24,45 @@ function counterReducer(state=100,action){
     }
 }
 
+
+// Add multiple reducer as key value pair
+let rootReducer = combineReducers({
+    // state name : reducer function
+    counter : counterReducer,
+    //cartItems : cartReducer
+    //product : productReducer
+})
+
 //Store
-let store =  createStore(counterReducer);
+//call reducer with @@redux/INIT to initialize default state
+let store =  createStore(rootReducer);
 
 export default store;
 
 
-//learning
+//--------------------learning -------------------------
+
+//subscribe take callback function as param
+// called after each dispatch execution
+
+store.subscribe(function callback(){
+    console.log("Subscribe");
+});
+
+// component did mount
+let unsubscripeFn  = store.subscribe(function callback(){
+    console.log("Subscribe 2");
+});
+
+// componenet will unmount
+unsubscripeFn();
+
 console.log("State ", store.getState());
 
-//Action is an object
+// Action is an object
+// type is a keyword
+// payload is not a keyword. It is conventional/best practice.we can use value:10 directly
+// instead of payload 
 let action = {
     type :INCREMENT,
     payload : {
@@ -35,6 +70,11 @@ let action = {
     }
 }
 
+console.log("Dispatch increment 10 ");
+// Dispatch 
+// calls the reducer
 store.dispatch(action);
-
 console.log("State after dispatch ", store.getState());
+
+console.log("Dispatch Unknown type");
+store.dispatch({type:"UNKNOWN"});
